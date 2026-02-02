3dbe37c93241ce3e2b9265d85248f712f51d326d
Half_nothing <Half_nothing@163.com>
2026-02-02 15:14:21 +0800

    refactor: 重构SubWindow使用AudioClientSignals并优化代码

--- src/ui/sub_window.py
+++ src/ui/sub_window.py
@@ -1,25 +1,27 @@
 #  Copyright (c) 2026 Half_nothing
 #  SPDX-License-Identifier: MIT
-from PySide6.QtCore import QPoint, Qt, SignalInstance
+from PySide6.QtCore import QPoint, Qt
 from PySide6.QtGui import QMouseEvent
 from PySide6.QtWidgets import QWidget
 
+from src.signal import AudioClientSignals
 from .form.generate.sub_window import Ui_SubWindow
-from ..signal.sub_window_signals import SubWindowSignals
 
 
 class SubWindow(QWidget, Ui_SubWindow):
-    def __init__(self, signals: SubWindowSignals):
+    def __init__(self, signals: AudioClientSignals):
         super().__init__()
         self.setupUi(self)
         self.setWindowFlag(Qt.WindowType.Tool | Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowSystemMenuHint
-                           | Qt.WindowType.WindowMinimizeButtonHint | Qt.WindowType.WindowMaximizeButtonHint | Qt.WindowType.WindowStaysOnTopHint)
+                           | Qt.WindowType.WindowMinimizeButtonHint | Qt.WindowType.WindowMaximizeButtonHint |
+                           Qt.WindowType.WindowStaysOnTopHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        self.signals = signals
         self.drag = False
         self.mouse_start_pt = QPoint()
         self.window_pos = QPoint()
 
-        self.signals = signals
         self.button_full_window.clicked.connect(self.full_window)
         self.signals.show_small_window.connect(self.show)
 
